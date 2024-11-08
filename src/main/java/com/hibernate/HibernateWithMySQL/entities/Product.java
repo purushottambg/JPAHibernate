c3b0f3b3f327fef94c0929727954e302469f3c3f
Purushottam Gutthe <purushottamgutthe6767@gmail.com>
2024-11-08 14:14:22 +0530

    Successfully created product table and also inserted few records also

--- src/main/java/com/hibernate/HibernateWithMySQL/entities/Product.java
+++ src/main/java/com/hibernate/HibernateWithMySQL/entities/Product.java
@@ -1,15 +1,55 @@
 package com.hibernate.HibernateWithMySQL.entities;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
 
 @Entity
 @Data
-@Table(name = "Product")
+@Builder
+@Table(name = "product_table",
+    uniqueConstraints = {
+        @UniqueConstraint(name = "product_cat", columnNames ="productCat" ),
+        @UniqueConstraint(name = "type_qty_price", columnNames = {"quantity", "type", "price"})
+}
+)
 @NoArgsConstructor
 @AllArgsConstructor
 public class Product {
+
+    @CreationTimestamp
+    private LocalDateTime RegisteredAt;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long productId;
+
+    @Column(length = 20, nullable = false)
+    private String prodName;
+
+    @Column(nullable = false)
+    private String productCat;
+
+    @Column(nullable = false)
+    private int quantity;
+
+    @Column(nullable = false)
+    private String type;
+
+    @Column(nullable = false)
+    private BigDecimal price;
+
+    @Column
+    private String prodDescription;
+
+    @UpdateTimestamp
+    private LocalDateTime lastUpdate;
+
 }
